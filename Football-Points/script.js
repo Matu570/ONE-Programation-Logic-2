6d7ca43aa0dcd156acad1381479d517abfce7da6
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-12-08 14:08:21 -0300

    Improving football points program

--- Football-Points/script.js
+++ Football-Points/script.js
@@ -9,4 +9,12 @@ function printPhrase(phrase){
 let victories=Number(prompt("How victories had your team?"));
 let ties=Number(prompt("How ties had your team?"));
 let teamPoints=victories*3+ties;
+let lastYearPoints=15;
 printPhrase(`<h3>The points total of your team is ${teamPoints}</h3>`);
+if(teamPoints>lastYearPoints){
+    printPhrase("<h2>The team is better than last year.</h2>");
+} else if(teamPoints==15){
+    printPhrase("<h2>The team is same than last year.</h2>")
+} else{
+    printPhrase("<h2>The team is worse than last year.</h2>")
+}
