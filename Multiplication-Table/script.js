6b7df58fc11e23bce62f111130b786c016ff9952
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-12-11 19:24:43 -0300

    Multiplication table

---
+++ Multiplication-Table/script.js
@@ -0,0 +1,29 @@
+function breakLine(){
+    document.write("<br><br><br>");
+};
+function printPhrase(phrase){
+    document.write(phrase);
+    breakLine();
+};
+function createMultiplicationTable(){
+    multiplicand=Number(prompt("What multiplication table do you want to see?"));
+    multiplier=1;
+    product=multiplicand;
+    while(multiplier<=10){
+        printPhrase(`${multiplicand} x ${multiplier} = ` + multiplicand*multiplier);
+        breakLine();
+        multiplier++
+    };
+};
+function createMultiplicationTable2(){
+    multiplicand=Number(prompt("What multiplication table do you want to see?"));
+    multiplier=1;
+    product=multiplicand;
+    for(i=1;i<=10;i++){
+        printPhrase(`${multiplicand} x ${multiplier} = ` + multiplicand*multiplier);
+        breakLine();
+        multiplier++
+    };
+};
+// createMultiplicationTable();
+createMultiplicationTable2();
